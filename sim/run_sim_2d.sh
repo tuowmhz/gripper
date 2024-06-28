5318c9b091481d87d9b94b0ab7faf083b982da61
siqi-shang <sqshang@link.cuhk.edu.hk>
2024-06-28 17:24:19 +0800

    Initial commit

---
+++ sim/run_sim_2d.sh
@@ -0,0 +1,9 @@
+model_root='./data/Icons-50.npy'     # directory containing 2d models
+save_dir='./sim/results'     # directory to save simulation results
+num_cpus=256     # number of cpus to use for parallel simulation
+
+for object_idx in {0..1000}; do     # number of objects
+    for ((i=0; i<1000; i+=512)) do      # number of manipulators
+        python sim/sim_2d.py $model_root $i $object_idx 512 1 $save_dir $num_cpus
+    done
+done
