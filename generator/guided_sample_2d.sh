a429c327a7f0164fbc8b46157041324a8ec75772
siqi-shang <siqi.shang@columbia.edu>
2024-09-10 21:42:02 -0500

    Update for desktop gpus

--- generator/guided_sample_2d.sh
+++ generator/guided_sample_2d.sh
@@ -1,4 +1,4 @@
 python generator/train.py --mode='test' --checkpoint_path='ckpts/dynamics_2d.pt' \
     --classifier_guidance --diffusion_checkpoint_path='ckpts/diffusion_2d.ckpt' --object_dir='data/Icons-50.npy' --save_dir='models/2d/generated_grippers' \
-    --ctrlpts_dim=14 --num_fingers=16 --grid_size=360 --num_pos=5 --object_max_num_vertices=100 \
-    --num_workers=0 --num_train_timesteps=15 --num_inference_steps=5 --ema_power=0.85 --batch_size=16  --num_cpus=32 --seed=0
+    --ctrlpts_dim=14 --num_fingers=2 --grid_size=360 --num_pos=5 --object_max_num_vertices=100 \
+    --num_workers=0 --num_train_timesteps=15 --num_inference_steps=5 --ema_power=0.85 --batch_size=16  --num_cpus=2 --seed=0
